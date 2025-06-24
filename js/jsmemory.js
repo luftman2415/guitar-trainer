313eba99bfbd569c8505d7b428d49c6947dc3703
luftman2415 <corazonesjuntos28@gmail.com>
2025-06-23 19:28:42 -0500

    Delete js directory

--- js/jsmemory.js
+++
@@ -1,9 +0,0 @@
-// Lógica específica para la Memoria Musical
-
-function setupMemory() {
-    // Aquí iría la lógica para el gestor de canciones
-    // Por ahora, solo mensajes en la consola para demostrar que funciona
-    document.getElementById('new-song-btn').addEventListener('click', () => alert('Función "Nueva Canción" no implementada.'));
-    document.getElementById('import-songs-btn').addEventListener('click', () => alert('Función "Importar" no implementada.'));
-    document.getElementById('export-songs-btn').addEventListener('click', () => alert('Función "Exportar" no implementada.'));
-}
